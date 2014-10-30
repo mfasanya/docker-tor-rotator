278ae5a9ab3243463e0ed81c3426e1f17dfea550
Mitchell Fasanya <m.fasanya@jsonwhois.com>
2014-10-30 08:54:34 +0000

    Increase to 40 processes

--- start.sh
+++ start.sh
@@ -21,6 +21,26 @@
 /usr/local/bin/tor --SocksPort 9068 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/18.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/18
 /usr/local/bin/tor --SocksPort 9069 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/19.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/19
 /usr/local/bin/tor --SocksPort 9070 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/20.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/20
+/usr/local/bin/tor --SocksPort 9071 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/21.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/21
+/usr/local/bin/tor --SocksPort 9072 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/22.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/22
+/usr/local/bin/tor --SocksPort 9073 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/23.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/23
+/usr/local/bin/tor --SocksPort 9074 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/24.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/24
+/usr/local/bin/tor --SocksPort 9075 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/25.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/25
+/usr/local/bin/tor --SocksPort 9076 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/26.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/26
+/usr/local/bin/tor --SocksPort 9077 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/27.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/27
+/usr/local/bin/tor --SocksPort 9078 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/28.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/28
+/usr/local/bin/tor --SocksPort 9079 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/29.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/29
+/usr/local/bin/tor --SocksPort 9080 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/30.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/30
+/usr/local/bin/tor --SocksPort 9081 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/31.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/31
+/usr/local/bin/tor --SocksPort 9082 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/32.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/32
+/usr/local/bin/tor --SocksPort 9083 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/33.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/33
+/usr/local/bin/tor --SocksPort 9084 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/34.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/34
+/usr/local/bin/tor --SocksPort 9085 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/35.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/35
+/usr/local/bin/tor --SocksPort 9086 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/36.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/36
+/usr/local/bin/tor --SocksPort 9087 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/37.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/37
+/usr/local/bin/tor --SocksPort 9088 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/38.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/38
+/usr/local/bin/tor --SocksPort 9089 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/39.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/39
+/usr/local/bin/tor --SocksPort 9090 --MaxCircuitDirtiness 10 --PidFile /var/run/tor/40.pid --RunAsDaemon 1 --DataDirectory /var/db/tor/40
 
 # launch haproxy
 haproxy -f /etc/default/haproxy.conf -q -db
